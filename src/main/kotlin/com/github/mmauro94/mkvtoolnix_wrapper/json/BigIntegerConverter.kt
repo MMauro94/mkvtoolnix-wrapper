bfe60c87c6c4d23c7bcb2d5c3606fbddd807e278
Mauro Molin <molin.mauro@gmail.com>
2021-10-28 21:50:24 +0200

    Upgrade klaxon library

--- src/main/kotlin/com/github/mmauro94/mkvtoolnix_wrapper/json/BigIntegerConverter.kt
+++ src/main/kotlin/com/github/mmauro94/mkvtoolnix_wrapper/json/BigIntegerConverter.kt
@@ -9,7 +9,18 @@ internal object BigIntegerConverter : Converter {
 
     override fun canConvert(cls: Class<*>) = cls == BigInteger::class.java
 
-    override fun fromJson(jv: JsonValue) = jv.long()?.toBigInteger() ?: jv.bigInteger
+    override fun fromJson(jv: JsonValue): BigInteger? {
+        return when (val long = jv.long()?.toBigInteger()) {
+            null -> {
+                when (val inside = jv.inside) {
+                    is BigInteger -> inside
+                    null -> null
+                    else -> throw KlaxonException("Invalid type ${jv.type} for BigInteger")
+                }
+            }
+            else -> long
+        }
+    }
 
     override fun toJson(value: Any): String {
         return if (value is BigInteger) value.toString()
